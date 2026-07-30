13bbfb8cb2762b0b354b6db8ef3c6f6e29107877
Ubuntu <ubuntu@instance-joe-jay.asia-southeast1-a.c.project-17837.internal>
2026-07-30 07:40:24 +0000

    feat: add offline token HTLC lifecycle

---
+++ lib/contract/htlc_token.go
@@ -0,0 +1,422 @@
+package contract
+
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/LoongYearMeta/tbc-contract-go/lib/util"
+	bt "github.com/LoongYearMeta/tbc-lib-go"
+	"github.com/LoongYearMeta/tbc-lib-go/bscript"
+	"github.com/LoongYearMeta/tbc-lib-go/crypto"
+)
+
+const (
+	htlcTokenCodeDust     = uint64(500)
+	htlcTokenContractDust = uint64(100)
+	htlcTokenFeePerKB     = 80
+)
+
+func htlcTokenTargetFee(tx *bt.Tx, extraBytes int) int {
+	size := tx.JSEstimateSize() + extraBytes
+	fee := (size*htlcTokenFeePerKB + 999) / 1000
+	if fee < htlcTokenFeePerKB {
+		return htlcTokenFeePerKB
+	}
+	return fee
+}
+
+func htlcTokenClassify(script *bscript.Script) (util.FTScriptInfo, error) {
+	info, err := util.ClassifyFTScript(script)
+	if err != nil {
+		return util.FTScriptInfo{}, fmt.Errorf("unsupported FT code: %w", err)
+	}
+	return info, nil
+}
+
+func htlcTokenTapeAt(tx *bt.Tx, codeVout int) (*bscript.Script, error) {
+	if tx == nil {
+		return nil, fmt.Errorf("nil FT parent transaction")
+	}
+	tapeVout := codeVout + 1
+	if codeVout < 0 || tapeVout >= len(tx.Outputs) {
+		return nil, fmt.Errorf("FT tape output %d is out of range", tapeVout)
+	}
+	return tx.Outputs[tapeVout].LockingScript, nil
+}
+
+func htlcTokenBalance(tape *bscript.Script) (*big.Int, error) {
+	if tape == nil {
+		return nil, fmt.Errorf("nil FT tape")
+	}
+	amount, err := util.GetFtBalanceFromTape(hex.EncodeToString(tape.Bytes()))
+	if err != nil {
+		return nil, err
+	}
+	if amount.Sign() <= 0 {
+		return nil, fmt.Errorf("FT tape encodes zero balance")
+	}
+	return amount, nil
+}
+
+func htlcTokenAddChange(tx *bt.Tx, address string, extraBytes int) error {
+	if err := tx.ChangeToAddress(address, newFeeQuote80()); err != nil {
+		return err
+	}
+	return tx.AdjustImplicitFeeToTarget(htlcTokenTargetFee(tx, extraBytes))
+}
+
+// DeployHTLCToken builds an unsigned transaction that atomically locks an FT
+// amount to an HTLC script. Inputs are [FT..., TBC fee].
+func DeployHTLCToken(
+	sender, receiver, hashlock string,
+	timelock uint32,
+	amount *big.Int,
+	ftUTXOs []*util.FtUTXO,
+	feeUTXO *bt.UTXO,
+	preTXs []*bt.Tx,
+	prePreTxData []string,
+) (string, error) {
+	if _, err := htlcAddressToPKH(sender); err != nil {
+		return "", fmt.Errorf("DeployHTLCToken: invalid sender: %w", err)
+	}
+	if _, err := htlcAddressToPKH(receiver); err != nil {
+		return "", fmt.Errorf("DeployHTLCToken: invalid receiver: %w", err)
+	}
+	if !util.IsValidSHA256Hash(hashlock) {
+		return "", fmt.Errorf("DeployHTLCToken: invalid hashlock")
+	}
+	if len(ftUTXOs) == 0 {
+		return "", fmt.Errorf("DeployHTLCToken: ftUTXOs must be non-empty")
+	}
+	if len(ftUTXOs) > 5 {
+		return "", fmt.Errorf("DeployHTLCToken: ftUTXOs length must be <= 5")
+	}
+	if len(preTXs) != len(ftUTXOs) || len(prePreTxData) != len(ftUTXOs) {
+		return "", fmt.Errorf("DeployHTLCToken: ancestry length must match ftUTXOs length")
+	}
+	if amount == nil || amount.Sign() <= 0 {
+		return "", fmt.Errorf("DeployHTLCToken: amount must be positive")
+	}
+	if feeUTXO == nil {
+		return "", fmt.Errorf("DeployHTLCToken: fee UTXO is required")
+	}
+
+	senderPKH, _ := htlcAddressToPKH(sender)
+	receiverPKH, _ := htlcAddressToPKH(receiver)
+	htlcScript, err := GetHTLCCode(senderPKH, receiverPKH, hashlock, timelock)
+	if err != nil {
+		return "", err
+	}
+	htlcHash := hex.EncodeToString(crypto.Hash160(crypto.Sha256(htlcScript.Bytes())))
+
+	firstInfo, err := htlcTokenClassify(ftUTXOs[0].LockingScript)
+	if err != nil {
+		return "", fmt.Errorf("DeployHTLCToken: %w", err)
+	}
+	tapeAmounts := make([]*big.Int, len(ftUTXOs))
+	total := new(big.Int)
+	var lockTimeMax uint32
+	for i, ftUTXO := range ftUTXOs {
+		if ftUTXO == nil || ftUTXO.FtBalance == nil || ftUTXO.FtBalance.Sign() < 0 {
+			return "", fmt.Errorf("DeployHTLCToken: invalid FT UTXO %d", i)
+		}
+		info, err := htlcTokenClassify(ftUTXO.LockingScript)
+		if err != nil {
+			return "", fmt.Errorf("DeployHTLCToken: FT input %d: %w", i, err)
+		}
+		if info != firstInfo {
+			return "", fmt.Errorf("DeployHTLCToken: FT input %d type differs from input 0", i)
+		}
+		tapeAmounts[i] = new(big.Int).Set(ftUTXO.FtBalance)
+		total.Add(total, ftUTXO.FtBalance)
+		if firstInfo.IsCoin {
+			tape, err := htlcTokenTapeAt(preTXs[i], int(ftUTXO.Vout))
+			if err != nil {
+				return "", fmt.Errorf("DeployHTLCToken: FT input %d: %w", i, err)
+			}
+			lockTime, err := GetLockTimeFromTape(tape)
+			if err != nil {
+				return "", fmt.Errorf("DeployHTLCToken: FT input %d locktime: %w", i, err)
+			}
+			if lockTime > lockTimeMax {
+				lockTimeMax = lockTime
+			}
+		}
+	}
+	if amount.Cmp(total) > 0 {
+		return "", fmt.Errorf("DeployHTLCToken: insufficient FT balance")
+	}
+
+	firstTape, err := htlcTokenTapeAt(preTXs[0], int(ftUTXOs[0].Vout))
+	if err != nil {
+		return "", fmt.Errorf("DeployHTLCToken: %w", err)
+	}
+	amountHex, changeHex := BuildTapeAmount(amount, tapeAmounts)
+	codeHex := hex.EncodeToString(ftUTXOs[0].LockingScript.Bytes())
+	tapeHex := hex.EncodeToString(firstTape.Bytes())
+
+	tx := newFTTx()
+	if err := tx.FromUTXOs(util.FtUTXOsToUTXOs(ftUTXOs)...); err != nil {
+		return "", err
+	}
+	if err := tx.FromUTXOs(feeUTXO); err != nil {
+		return "", err
+	}
+	tx.AddOutput(&bt.Output{LockingScript: htlcScript, Satoshis: htlcTokenContractDust})
+	lockedCode, err := BuildFTtransferCode(codeHex, htlcHash)
+	if err != nil {
+		return "", err
+	}
+	lockedTape, err := BuildFTtransferTape(tapeHex, amountHex)
+	if err != nil {
+		return "", err
+	}
+	tx.AddOutput(&bt.Output{LockingScript: lockedCode, Satoshis: htlcTokenCodeDust})
+	tx.AddOutput(&bt.Output{LockingScript: lockedTape, Satoshis: 0})
+	if amount.Cmp(total) < 0 {
+		changeCode, err := BuildFTtransferCode(codeHex, sender)
+		if err != nil {
+			return "", err
+		}
+		changeTape, err := BuildFTtransferTape(tapeHex, changeHex)
+		if err != nil {
+			return "", err
+		}
+		tx.AddOutput(&bt.Output{LockingScript: changeCode, Satoshis: htlcTokenCodeDust})
+		tx.AddOutput(&bt.Output{LockingScript: changeTape, Satoshis: 0})
+	}
+	if firstInfo.IsCoin {
+		for i := range ftUTXOs {
+			tx.Inputs[i].SequenceNumber = 0xfffffffe
+		}
+		tx.LockTime = lockTimeMax
+	}
+	if err := htlcTokenAddChange(tx, sender, len(ftUTXOs)*2000); err != nil {
+		return "", fmt.Errorf("DeployHTLCToken: change/fee: %w", err)
+	}
+	return tx.String(), nil
+}
+
+// FillSigDeployHTLCToken fills the FT and final P2PKH fee inputs.
+func FillSigDeployHTLCToken(
+	raw string,
+	sigs []string,
+	publicKey string,
+	preTXs []*bt.Tx,
+	prePreTxData []string,
+) (string, error) {
+	tx, err := bt.NewTxFromString(raw)
+	if err != nil {
+		return "", fmt.Errorf("FillSigDeployHTLCToken: invalid raw transaction: %w", err)
+	}
+	if len(preTXs) == 0 || len(preTXs) != len(prePreTxData) {
+		return "", fmt.Errorf("FillSigDeployHTLCToken: preTX/prePreTxData length mismatch")
+	}
+	if len(sigs) != len(preTXs)+1 {
+		return "", fmt.Errorf("FillSigDeployHTLCToken: sigs length must be %d", len(preTXs)+1)
+	}
+	if len(tx.Inputs) != len(sigs) {
+		return "", fmt.Errorf("FillSigDeployHTLCToken: transaction input count mismatch")
+	}
+	firstVout := int(tx.Inputs[0].PreviousTxOutIndex)
+	if firstVout < 0 || firstVout >= len(preTXs[0].Outputs) {
+		return "", fmt.Errorf("FillSigDeployHTLCToken: FT input vout out of range")
+	}
+	info, err := htlcTokenClassify(preTXs[0].Outputs[firstVout].LockingScript)
+	if err != nil {
+		return "", err
+	}
+	for i := range preTXs {
+		vout := int(tx.Inputs[i].PreviousTxOutIndex)
+		unlock, err := StaticGetFTunlock(
+			sigs[i], publicKey, tx, preTXs[i], prePreTxData[i],
+			i, vout, info.IsCoin,
+		)
+		if err != nil {
+			return "", fmt.Errorf("FillSigDeployHTLCToken: FT input %d: %w", i, err)
+		}
+		if err := tx.InsertInputUnlockingScript(uint32(i), unlock); err != nil {
+			return "", err
+		}
+	}
+	feeUnlock, err := bscript.NewFromASM(sigs[len(preTXs)] + " " + publicKey)
+	if err != nil {
+		return "", err
+	}
+	if err := tx.InsertInputUnlockingScript(uint32(len(preTXs)), feeUnlock); err != nil {
+		return "", err
+	}
+	return tx.String(), nil
+}
+
+func buildHTLCTokenRedemption(
+	address string,
+	htlcUTXO *bt.UTXO,
+	ftUTXO *util.FtUTXO,
+	deployTX *bt.Tx,
+	feeUTXO *bt.UTXO,
+	refundLockTime *uint32,
+) (string, error) {
+	if _, err := htlcAddressToPKH(address); err != nil {
+		return "", fmt.Errorf("invalid redemption address: %w", err)
+	}
+	if htlcUTXO == nil || ftUTXO == nil || feeUTXO == nil || deployTX == nil {
+		return "", fmt.Errorf("HTLC, FT, deploy, and fee inputs are required")
+	}
+	info, err := htlcTokenClassify(ftUTXO.LockingScript)
+	if err != nil {
+		return "", err
+	}
+	tape, err := htlcTokenTapeAt(deployTX, int(ftUTXO.Vout))
+	if err != nil {
+		return "", err
+	}
+	total, err := htlcTokenBalance(tape)
+	if err != nil {
+		return "", err
+	}
+	amountHex, _ := BuildTapeAmountWithFtInputIndex(total, []*big.Int{total}, 1)
+	codeOut, err := BuildFTtransferCode(hex.EncodeToString(ftUTXO.LockingScript.Bytes()), address)
+	if err != nil {
+		return "", err
+	}
+	tapeOut, err := BuildFTtransferTape(hex.EncodeToString(tape.Bytes()), amountHex)
+	if err != nil {
+		return "", err
+	}
+
+	tx := newFTTx()
+	if err := tx.FromUTXOs(htlcUTXO, util.FtUTXOToUTXO(ftUTXO), feeUTXO); err != nil {
+		return "", err
+	}
+	tx.AddOutput(&bt.Output{LockingScript: codeOut, Satoshis: htlcTokenCodeDust})
+	tx.AddOutput(&bt.Output{LockingScript: tapeOut, Satoshis: 0})
+
+	var coinLockTime uint32
+	if info.IsCoin {
+		tx.Inputs[1].SequenceNumber = 0xfffffffe
+		coinLockTime, err = GetLockTimeFromTape(tape)
+		if err != nil {
+			return "", err
+		}
+	}
+	if refundLockTime != nil {
+		tx.Inputs[0].SequenceNumber = 0xfffffffe
+		tx.LockTime = *refundLockTime
+		if coinLockTime > tx.LockTime {
+			tx.LockTime = coinLockTime
+		}
+	} else if info.IsCoin {
+		tx.LockTime = coinLockTime
+	}
+	if err := htlcTokenAddChange(tx, address, 3000); err != nil {
+		return "", fmt.Errorf("redemption change/fee: %w", err)
+	}
+	return tx.String(), nil
+}
+
+// WithdrawHTLCToken builds an unsigned secret-branch redemption.
+func WithdrawHTLCToken(
+	receiver string,
+	htlcUTXO *bt.UTXO,
+	ftUTXO *util.FtUTXO,
+	deployTX *bt.Tx,
+	feeUTXO *bt.UTXO,
+) (string, error) {
+	return buildHTLCTokenRedemption(receiver, htlcUTXO, ftUTXO, deployTX, feeUTXO, nil)
+}
+
+// RefundHTLCToken builds an unsigned timelock-branch redemption.
+func RefundHTLCToken(
+	sender string,
+	htlcUTXO *bt.UTXO,
+	ftUTXO *util.FtUTXO,
+	deployTX *bt.Tx,
+	feeUTXO *bt.UTXO,
+	timelock uint32,
+) (string, error) {
+	return buildHTLCTokenRedemption(sender, htlcUTXO, ftUTXO, deployTX, feeUTXO, &timelock)
+}
+
+func fillSigHTLCTokenRedemption(
+	raw string,
+	sigs []string,
+	publicKey string,
+	secret *string,
+	deployTX *bt.Tx,
+	prePreTxData string,
+) (string, error) {
+	tx, err := bt.NewTxFromString(raw)
+	if err != nil {
+		return "", fmt.Errorf("invalid redemption raw transaction: %w", err)
+	}
+	if len(sigs) != 3 || len(tx.Inputs) != 3 {
+		return "", fmt.Errorf("sigs must contain [HTLC, FTCode, tbcFee]")
+	}
+	if deployTX == nil || len(deployTX.Outputs) < 3 {
+		return "", fmt.Errorf("deploy transaction is missing FT outputs")
+	}
+	info, err := htlcTokenClassify(deployTX.Outputs[1].LockingScript)
+	if err != nil {
+		return "", err
+	}
+
+	branch := "OP_FALSE"
+	if secret != nil {
+		if strings.TrimSpace(*secret) == "" {
+			return "", fmt.Errorf("secret is required")
+		}
+		branch = *secret + " OP_TRUE"
+	}
+	htlcUnlock, err := bscript.NewFromASM(sigs[0] + " " + publicKey + " " + branch)
+	if err != nil {
+		return "", err
+	}
+	if err := tx.InsertInputUnlockingScript(0, htlcUnlock); err != nil {
+		return "", err
+	}
+
+	ftVout := int(tx.Inputs[1].PreviousTxOutIndex)
+	ftUnlock, err := StaticGetFTUnlockSwap(
+		sigs[1], publicKey, tx, deployTX, prePreTxData, deployTX,
+		1, ftVout, info.Version, info.IsCoin, false,
+	)
+	if err != nil {
+		return "", fmt.Errorf("fill FT unlock: %w", err)
+	}
+	if err := tx.InsertInputUnlockingScript(1, ftUnlock); err != nil {
+		return "", err
+	}
+	feeUnlock, err := bscript.NewFromASM(sigs[2] + " " + publicKey)
+	if err != nil {
+		return "", err
+	}
+	if err := tx.InsertInputUnlockingScript(2, feeUnlock); err != nil {
+		return "", err
+	}
+	return tx.String(), nil
+}
+
+// FillSigWithdrawHTLCToken fills [HTLC, FTCode, fee] signatures.
+func FillSigWithdrawHTLCToken(
+	raw string,
+	sigs []string,
+	publicKey, secret string,
+	deployTX *bt.Tx,
+	prePreTxData string,
+) (string, error) {
+	return fillSigHTLCTokenRedemption(raw, sigs, publicKey, &secret, deployTX, prePreTxData)
+}
+
+// FillSigRefundHTLCToken fills [HTLC, FTCode, fee] signatures.
+func FillSigRefundHTLCToken(
+	raw string,
+	sigs []string,
+	publicKey string,
+	deployTX *bt.Tx,
+	prePreTxData string,
+) (string, error) {
+	return fillSigHTLCTokenRedemption(raw, sigs, publicKey, nil, deployTX, prePreTxData)
+}
---
+++ lib/contract/htlc_token_test.go
@@ -0,0 +1,265 @@
+package contract
+
+import (
+	"bytes"
+	"encoding/hex"
+	"math/big"
+	"strings"
+	"testing"
+
+	"github.com/LoongYearMeta/tbc-contract-go/lib/util"
+	bt "github.com/LoongYearMeta/tbc-lib-go"
+	"github.com/LoongYearMeta/tbc-lib-go/bec"
+	"github.com/LoongYearMeta/tbc-lib-go/bscript"
+)
+
+type htlcTokenFixture struct {
+	senderKey   *bec.PrivateKey
+	receiverKey *bec.PrivateKey
+	sender      string
+	receiver    string
+	hashlock    string
+	code        *bscript.Script
+	tape        *bscript.Script
+	ftUTXO      *util.FtUTXO
+	feeUTXO     *bt.UTXO
+	preTX       *bt.Tx
+}
+
+func newHTLCTokenFixture(t *testing.T, balance int64) htlcTokenFixture {
+	t.Helper()
+	senderKey := mustTestPrivateKey(t, 21)
+	receiverKey := mustTestPrivateKey(t, 22)
+	senderAddr, err := bscript.NewAddressFromPublicKey(senderKey.PubKey(), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	receiverAddr, err := bscript.NewAddressFromPublicKey(receiverKey.PubKey(), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	amountHex, _ := BuildTapeAmount(big.NewInt(balance), []*big.Int{big.NewInt(balance)})
+	tape, err := bscript.NewFromASM("OP_FALSE OP_RETURN " + amountHex)
+	if err != nil {
+		t.Fatal(err)
+	}
+	code, err := getFTmintCode(strings.Repeat("31", 32), 0, senderAddr.AddressString, len(tape.Bytes()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	preTX := bt.NewTx()
+	preTX.AddOutput(&bt.Output{LockingScript: code, Satoshis: 500})
+	preTX.AddOutput(&bt.Output{LockingScript: tape, Satoshis: 0})
+	ftTxID, err := hex.DecodeString(preTX.TxID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	feeScript, err := bscript.NewP2PKHFromAddress(senderAddr.AddressString)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return htlcTokenFixture{
+		senderKey:   senderKey,
+		receiverKey: receiverKey,
+		sender:      senderAddr.AddressString,
+		receiver:    receiverAddr.AddressString,
+		hashlock:    hex.EncodeToString(bytes.Repeat([]byte{0x42}, 32)),
+		code:        code,
+		tape:        tape,
+		ftUTXO: &util.FtUTXO{
+			TxID:          ftTxID,
+			Vout:          0,
+			LockingScript: code,
+			Satoshis:      500,
+			FtBalance:     big.NewInt(balance),
+		},
+		feeUTXO: &bt.UTXO{
+			TxID:          bytes.Repeat([]byte{0x51}, 32),
+			Vout:          1,
+			LockingScript: feeScript,
+			Satoshis:      20_000,
+		},
+		preTX: preTX,
+	}
+}
+
+func TestDeployHTLCTokenValidatesInputs(t *testing.T) {
+	fx := newHTLCTokenFixture(t, 1_000)
+	tests := []struct {
+		name  string
+		utxos []*util.FtUTXO
+		preTX []*bt.Tx
+		pre   []string
+		amt   *big.Int
+		want  string
+	}{
+		{"empty", nil, nil, nil, big.NewInt(1), "non-empty"},
+		{"too many", []*util.FtUTXO{fx.ftUTXO, fx.ftUTXO, fx.ftUTXO, fx.ftUTXO, fx.ftUTXO, fx.ftUTXO}, make([]*bt.Tx, 6), make([]string, 6), big.NewInt(1), "<= 5"},
+		{"ancestry", []*util.FtUTXO{fx.ftUTXO}, nil, nil, big.NewInt(1), "length"},
+		{"insufficient", []*util.FtUTXO{fx.ftUTXO}, []*bt.Tx{fx.preTX}, []string{""}, big.NewInt(1_001), "insufficient"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := DeployHTLCToken(
+				fx.sender, fx.receiver, fx.hashlock, 900_000,
+				tt.amt, tt.utxos, fx.feeUTXO, tt.preTX, tt.pre,
+			)
+			if err == nil || !strings.Contains(strings.ToLower(err.Error()), strings.ToLower(tt.want)) {
+				t.Fatalf("unexpected error: %v", err)
+			}
+		})
+	}
+}
+
+func TestDeployHTLCTokenOutputLayout(t *testing.T) {
+	fx := newHTLCTokenFixture(t, 1_000)
+	raw, err := DeployHTLCToken(
+		fx.sender, fx.receiver, fx.hashlock, 900_000,
+		big.NewInt(600), []*util.FtUTXO{fx.ftUTXO}, fx.feeUTXO,
+		[]*bt.Tx{fx.preTX}, []string{""},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx := mustTx(t, raw)
+	if len(tx.Inputs) != 2 {
+		t.Fatalf("inputs = %d, want FT + fee", len(tx.Inputs))
+	}
+	if len(tx.Outputs) != 6 {
+		t.Fatalf("outputs = %d, want HTLC + locked pair + change pair + fee change", len(tx.Outputs))
+	}
+	if tx.Outputs[0].Satoshis != 100 || tx.Outputs[1].Satoshis != 500 || tx.Outputs[2].Satoshis != 0 {
+		t.Fatalf("unexpected locked output values")
+	}
+	if len(tx.Outputs[1].LockingScript.Bytes()) != 1884 {
+		t.Fatalf("locked FT code length = %d", len(tx.Outputs[1].LockingScript.Bytes()))
+	}
+	lockedBalance, err := util.GetFtBalanceFromTape(hex.EncodeToString(tx.Outputs[2].LockingScript.Bytes()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	changeBalance, err := util.GetFtBalanceFromTape(hex.EncodeToString(tx.Outputs[4].LockingScript.Bytes()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if lockedBalance.Cmp(big.NewInt(600)) != 0 || changeBalance.Cmp(big.NewInt(400)) != 0 {
+		t.Fatalf("balances locked/change = %s/%s", lockedBalance, changeBalance)
+	}
+}
+
+func TestFillSigDeployHTLCTokenFillsEveryInput(t *testing.T) {
+	fx := newHTLCTokenFixture(t, 1_000)
+	raw, err := DeployHTLCToken(
+		fx.sender, fx.receiver, fx.hashlock, 900_000,
+		big.NewInt(600), []*util.FtUTXO{fx.ftUTXO}, fx.feeUTXO,
+		[]*bt.Tx{fx.preTX}, []string{""},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pubKey := hex.EncodeToString(fx.senderKey.PubKey().SerialiseCompressed())
+	filled, err := FillSigDeployHTLCToken(raw, []string{"aa", "bb"}, pubKey, []*bt.Tx{fx.preTX}, []string{""})
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx := mustTx(t, filled)
+	for i, input := range tx.Inputs {
+		if len(input.UnlockingScript.Bytes()) == 0 {
+			t.Fatalf("input %d remains unsigned", i)
+		}
+	}
+}
+
+func deployedHTLCTokenFixture(t *testing.T) (htlcTokenFixture, *bt.Tx, *bt.UTXO, *util.FtUTXO) {
+	t.Helper()
+	fx := newHTLCTokenFixture(t, 1_000)
+	raw, err := DeployHTLCToken(
+		fx.sender, fx.receiver, fx.hashlock, 900_000,
+		big.NewInt(600), []*util.FtUTXO{fx.ftUTXO}, fx.feeUTXO,
+		[]*bt.Tx{fx.preTX}, []string{""},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx := mustTx(t, raw)
+	txid, err := hex.DecodeString(tx.TxID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	htlcUTXO := &bt.UTXO{
+		TxID:          txid,
+		Vout:          0,
+		LockingScript: tx.Outputs[0].LockingScript,
+		Satoshis:      tx.Outputs[0].Satoshis,
+	}
+	ftUTXO := &util.FtUTXO{
+		TxID:          txid,
+		Vout:          1,
+		LockingScript: tx.Outputs[1].LockingScript,
+		Satoshis:      tx.Outputs[1].Satoshis,
+		FtBalance:     big.NewInt(600),
+	}
+	return fx, tx, htlcUTXO, ftUTXO
+}
+
+func TestWithdrawHTLCTokenBuildsThreeInputRedemption(t *testing.T) {
+	fx, deployTX, htlcUTXO, ftUTXO := deployedHTLCTokenFixture(t)
+	raw, err := WithdrawHTLCToken(fx.receiver, htlcUTXO, ftUTXO, deployTX, fx.feeUTXO)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx := mustTx(t, raw)
+	if len(tx.Inputs) != 3 || len(tx.Outputs) != 3 {
+		t.Fatalf("shape = %d inputs/%d outputs", len(tx.Inputs), len(tx.Outputs))
+	}
+	if tx.LockTime != 0 {
+		t.Fatalf("withdraw locktime = %d", tx.LockTime)
+	}
+}
+
+func TestRefundHTLCTokenSetsSequenceAndLockTime(t *testing.T) {
+	fx, deployTX, htlcUTXO, ftUTXO := deployedHTLCTokenFixture(t)
+	raw, err := RefundHTLCToken(fx.sender, htlcUTXO, ftUTXO, deployTX, fx.feeUTXO, 900_000)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tx := mustTx(t, raw)
+	if tx.Inputs[0].SequenceNumber != 0xfffffffe || tx.LockTime != 900_000 {
+		t.Fatalf("refund sequence/locktime = %x/%d", tx.Inputs[0].SequenceNumber, tx.LockTime)
+	}
+}
+
+func TestFillSigWithdrawAndRefundHTLCTokenFillThreeInputs(t *testing.T) {
+	fx, deployTX, htlcUTXO, ftUTXO := deployedHTLCTokenFixture(t)
+	pubKey := hex.EncodeToString(fx.receiverKey.PubKey().SerialiseCompressed())
+	secret := "1234"
+	sigs := []string{"aa", "bb", "cc"}
+
+	withdrawRaw, err := WithdrawHTLCToken(fx.receiver, htlcUTXO, ftUTXO, deployTX, fx.feeUTXO)
+	if err != nil {
+		t.Fatal(err)
+	}
+	withdrawFilled, err := FillSigWithdrawHTLCToken(withdrawRaw, sigs, pubKey, secret, deployTX, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, input := range mustTx(t, withdrawFilled).Inputs {
+		if len(input.UnlockingScript.Bytes()) == 0 {
+			t.Fatalf("withdraw input %d remains unsigned", i)
+		}
+	}
+
+	refundRaw, err := RefundHTLCToken(fx.sender, htlcUTXO, ftUTXO, deployTX, fx.feeUTXO, 900_000)
+	if err != nil {
+		t.Fatal(err)
+	}
+	refundFilled, err := FillSigRefundHTLCToken(refundRaw, sigs, pubKey, deployTX, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, input := range mustTx(t, refundFilled).Inputs {
+		if len(input.UnlockingScript.Bytes()) == 0 {
+			t.Fatalf("refund input %d remains unsigned", i)
+		}
+	}
+}
